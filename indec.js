115ce01d2b5c01db4c107eb83260661c8e6006b4
Dipti ranjan sahoo <sahoo.diptiranjan.itm020@gmail.com>
2025-07-06 23:08:06 +0530

    this is js code for traffic

---
+++ indec.js
@@ -0,0 +1,11 @@
+// console.log("Hello, World!");   
+
+let age = 30;
+let grace = 2;
+console.log("Your age is: " + age);
+if(age-grace > 18 ){
+    console.log("You are an adult.");
+}
+else{
+    console.log("You are a minor.");
+}
