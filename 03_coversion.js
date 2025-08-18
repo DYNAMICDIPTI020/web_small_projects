0e4281cb9c783df7cf6680a15758b85381c9bd57
Dipti ranjan sahoo <sahoo.diptiranjan.itm020@gmail.com>
2025-08-18 23:43:57 +0530

    Add files via upload

---
+++ 03_coversion.js
@@ -0,0 +1,31 @@
+// let score = "33jg"
+
+// console.log(typeof score); // string
+// console.log(typeof(score)); // number
+
+// let valueNumber = Number(score);
+// console.log(typeof valueNumber); // number
+// console.log(valueNumber); // NaN
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); // true
+
+// let name = "kanha"
+// let booleanName = Boolean(name);
+// let numberName = Number(name);
+// let stringName = String(name);
+// console.log(booleanName); // true
+// console.log(numberName); // NaN
+// console.log(stringName); // "kanha"
+
+// // 1 => true ; 0 => false
+// // "" => false
+// // "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber); // string
+console.log(stringNumber); // "33" 
