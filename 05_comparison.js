9d91ff8931205c9b298cd8bb9b64d9b904fbb4b5
Dipti ranjan sahoo <sahoo.diptiranjan.itm020@gmail.com>
2025-08-20 21:50:12 +0530

    this is a code for comparison

---
+++ 05_comparison.js
@@ -0,0 +1,18 @@
+console.log(5 > 3);
+console.log(2 >= 1);
+console.log(1 < 2);
+console.log(3 === 3);
+console.log(4 !== 5);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null === 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);//false
+console.log(undefined < 0);//false
+console.log(undefined == 0);//false
+console.log(undefined === 0);//false
+console.log(undefined != 0);//true
+console.log(undefined !== 0);//true
+console.log(undefined >= 0);//false
+console.log(undefined <= 0);//false
